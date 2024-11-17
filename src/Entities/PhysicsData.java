2c9d8ff985dbd3bac3b6f5c952ed5b6586cf44e4
abdulmajeed <abdullmaged98916437>
2024-11-17 14:46:16 +0400

    Create Entity with PhysicsData class including Setter and Getter

---
+++ src/Entities/PhysicsData.java
@@ -0,0 +1,38 @@
+package Entities;
+
+public class PhysicsData {
+    private double mass;
+    private double acceleration;
+    private MotionData motionData;
+
+    //This is the Constructor
+    public PhysicsData(double mass, double acceleration, MotionData motionData) {
+        this.mass = mass;
+        this.acceleration = acceleration;
+        this.motionData = motionData;
+    }
+
+    public double getMass() {
+        return mass;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(double acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public MotionData getMotionData() {
+        return motionData;
+    }
+
+    public void setMotionData(MotionData motionData) {
+        this.motionData = motionData;
+    }
+}
