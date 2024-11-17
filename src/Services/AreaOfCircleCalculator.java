b3e2b6b1300aac791daaff06015839c967dc6a61
abdulmajeed <abdullmaged98916437>
2024-11-17 17:29:27 +0400

    Create Services and Implement  FinalVelociytSquaredCalculator with CircleData and Calculator interface

---
+++ src/Services/AreaOfCircleCalculator.java
@@ -0,0 +1,20 @@
+package Services;
+
+import Entities.CircleData;
+import Interfaces.CalculatorInterface;
+
+public class AreaOfCircleCalculator implements CalculatorInterface {
+    private  CircleData circleData;
+    private AreaOfCircleCalculator areaOfCircleCalculator;
+
+    public AreaOfCircleCalculator(CircleData circleData) {
+        this.circleData = circleData;
+
+    }
+
+    @Override
+    public void calculate() {
+        double area = Math.PI * Math.pow(circleData.getRadius(), 2);
+        System.out.println("Area of Circle (A) = " + area);
+    }
+}
