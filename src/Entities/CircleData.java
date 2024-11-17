3b8a7930c82da12c21acf6a320ab684f2546a2ae
abdulmajeed <abdullmaged98916437>
2024-11-17 15:04:59 +0400

    Create Entity with CircleData class including Setter and Getter

---
+++ src/Entities/CircleData.java
@@ -0,0 +1,28 @@
+package Entities;
+
+public class CircleData {
+    private double radius;
+    private BaseData baseData;
+
+    //This is a Constructor
+    public CircleData(double radius, BaseData baseData) {
+        this.radius = radius;
+        this.baseData = baseData;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public BaseData getBaseData() {
+        return baseData;
+    }
+
+    public void setBaseData(BaseData baseData) {
+        this.baseData = baseData;
+    }
+}
