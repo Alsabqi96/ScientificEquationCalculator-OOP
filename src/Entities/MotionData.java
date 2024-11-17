5edaedc449ee8ca13748e316d5a897d8e087a6c7
abdulmajeed <abdullmaged98916437>
2024-11-17 14:38:40 +0400

    Create Entity with MotionData class including Setter and Getter

---
+++ src/Entities/MotionData.java
@@ -0,0 +1,48 @@
+package Entities;
+
+public class MotionData {
+    private double initialVelocity;
+    private double acceleration;
+    private double time;
+    private BaseData baseData;
+
+    //This is the Constructor
+    public MotionData(double initialVelocity, double acceleration, double time, BaseData baseData) {
+        this.initialVelocity = initialVelocity;
+        this.acceleration = acceleration;
+        this.time = time;
+        this.baseData = baseData;
+    }
+
+    public double getInitialVelocity() {
+        return initialVelocity;
+    }
+
+    public void setInitialVelocity(double initialVelocity) {
+        this.initialVelocity = initialVelocity;
+    }
+
+    public double getAcceleration() {
+        return acceleration;
+    }
+
+    public void setAcceleration(double acceleration) {
+        this.acceleration = acceleration;
+    }
+
+    public double getTime() {
+        return time;
+    }
+
+    public void setTime(double time) {
+        this.time = time;
+    }
+
+    public BaseData getBaseData() {
+        return baseData;
+    }
+
+    public void setBaseData(BaseData baseData) {
+        this.baseData = baseData;
+    }
+}
